b21fc707e3cad79464d3436fe2fec35298e11410
kounou25 <kounougilbert288@gmail.com>
2024-12-20 18:04:32 +0100

    version stable

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true, // Désactive ESLint durant la construction
+    },
+  };
+  
+  export default nextConfig;
+  
